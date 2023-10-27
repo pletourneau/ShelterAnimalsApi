90d0905d4f1aee3a5d0311cde559564b7cec58fe
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 09:19:44 -0700

    add initial migration and seed data

--- ShelterAnimalsApi/Models/ShelterAnimalsApiContext.cs
+++ ShelterAnimalsApi/Models/ShelterAnimalsApiContext.cs
@@ -9,5 +9,16 @@ namespace ShelterAnimalsApi.Models
     public ShelterAnimalsApiContext(DbContextOptions<ShelterAnimalsApiContext> options) : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "Matilda", Species = "Dog", Age = 7 },
+          new Animal { AnimalId = 2, Name = "Rexie", Species = "Dog", Age = 10 },
+          new Animal { AnimalId = 3, Name = "Matilda", Species = "Cat", Age = 2 },
+          new Animal { AnimalId = 4, Name = "Pip", Species = "Cat", Age = 4 },
+          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Cat", Age = 12 }
+        );
+    }
   }
 }
