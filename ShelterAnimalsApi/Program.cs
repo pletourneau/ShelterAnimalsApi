1ce7b5080cc8982a9cafdab3af058c72638d019e
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 09:16:00 -0700

    fix build errors in program.cs

--- ShelterAnimalsApi/Program.cs
+++ ShelterAnimalsApi/Program.cs
@@ -1,10 +1,13 @@
+using ShelterAnimalsApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddDbContext<CretaceousApiContext>(
+builder.Services.AddDbContext<ShelterAnimalsApiContext>(
                   dbContextOptions => dbContextOptions
                     .UseMySql(
                       builder.Configuration["ConnectionStrings:DefaultConnection"], 
