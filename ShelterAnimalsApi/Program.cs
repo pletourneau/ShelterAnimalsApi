b9674a1daf630dc56e1afb818b10daa69c079a61
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 14:10:36 -0700

    WIP

--- ShelterAnimalsApi/Program.cs
+++ ShelterAnimalsApi/Program.cs
@@ -1,11 +1,23 @@
 using ShelterAnimalsApi.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true;
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<ShelterAnimalsApiContext>(
                   dbContextOptions => dbContextOptions
