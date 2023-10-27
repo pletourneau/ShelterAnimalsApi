d2c64c83d31ea3714feeebdbfa672c0f59c7c159
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 09:40:33 -0700

    add post route to animalscontroller

--- ShelterAnimalsApi/Controllers/AnimalsController.cs
+++ ShelterAnimalsApi/Controllers/AnimalsController.cs
@@ -35,6 +35,14 @@ namespace ShelterAnimalsApi.Controllers
 
       return animal;
     }
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
   }
 }
 
