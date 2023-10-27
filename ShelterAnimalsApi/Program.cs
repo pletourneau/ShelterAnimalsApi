48b31ee8fdf98f20fed8ef2dcd0048eb71712028
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 08:59:35 -0700

    finish boilerplate

--- ShelterAnimalsApi/Program.cs
+++ ShelterAnimalsApi/Program.cs
@@ -15,7 +15,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+else 
+{
+  app.UseHttpsRedirection();
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
