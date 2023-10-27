6c1e3ad863b78d89dd5d42a1ff8d2b77ef222a91
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 09:35:30 -0700

    add breed to model/context

---
+++ ShelterAnimalsApi/Migrations/20231027163411_AddBreedToModelAndContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShelterAnimalsApi.Migrations
+{
+    public partial class AddBreedToModelAndContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Breed",
+                table: "Animals",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                column: "Breed",
+                value: "Bichon");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                column: "Breed",
+                value: "Boxer");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                column: "Breed",
+                value: "Persian");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                column: "Breed",
+                value: "American Short-Hair");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                column: "Breed",
+                value: "Calico");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Breed",
+                table: "Animals");
+        }
+    }
+}
--- ShelterAnimalsApi/Models/ShelterAnimalsApiContext.cs
+++ ShelterAnimalsApi/Models/ShelterAnimalsApiContext.cs
@@ -13,11 +13,11 @@ namespace ShelterAnimalsApi.Models
     {
       builder.Entity<Animal>()
         .HasData(
-          new Animal { AnimalId = 1, Name = "Matilda", Species = "Dog", Age = 7 },
-          new Animal { AnimalId = 2, Name = "Rexie", Species = "Dog", Age = 10 },
-          new Animal { AnimalId = 3, Name = "Matilda", Species = "Cat", Age = 2 },
-          new Animal { AnimalId = 4, Name = "Pip", Species = "Cat", Age = 4 },
-          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Cat", Age = 12 }
+          new Animal { AnimalId = 1, Name = "Matilda", Species = "Dog", Breed = "Bichon", Age = 7 },
+          new Animal { AnimalId = 2, Name = "Rexie", Species = "Dog", Breed = "Boxer", Age = 10 },
+          new Animal { AnimalId = 3, Name = "Matilda", Species = "Cat", Breed = "Persian", Age = 2 },
+          new Animal { AnimalId = 4, Name = "Pip", Species = "Cat", Breed = "American Short-Hair", Age = 4 },
+          new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Cat", Breed = "Calico", Age = 12 }
         );
     }
   }
