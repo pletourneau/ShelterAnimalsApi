8af5421310e35ae8b0bf535aeafc964a6c02506b
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 09:10:10 -0700

    update program.cs file

--- ShelterAnimalsApi/Program.cs
+++ ShelterAnimalsApi/Program.cs
@@ -4,6 +4,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<CretaceousApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
