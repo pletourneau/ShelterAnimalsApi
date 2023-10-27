bdf6d312502daf91c245b97a331b23d1e928f9eb
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-27 10:09:43 -0700

    add data annotations

--- ShelterAnimalsApi/Models/Animal.cs
+++ ShelterAnimalsApi/Models/Animal.cs
@@ -1,11 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShelterAnimalsApi.Models
 {
   public class Animal
   {
     public int AnimalId { get; set; }
+    
+    [Required]
+    [StringLength(20)]
+
     public string Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
+
     public string Species { get; set; }
+
+    [Required]
+    [StringLength(20)]
+
     public string Breed { get; set; }
+
+    [Required]
+    [Range(0, 50, ErrorMessage = "Age must be between 0 and 50.")]
+
     public int Age { get; set; }
   }
 }
